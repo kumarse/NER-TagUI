8df82313ab1f731ab5a9ebd1daefdfa30f3c1537
satheeshravir <satheeshravir@gmail.com>
2016-04-07 21:14:30 -0700

    Added file save route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var jsonfile = require('jsonfile')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,5 +17,18 @@ router.get('/list', function(req, res, next) {
   	
 });
 
+router.post('/save', function(req, res, next) {
+	fileName = req.body.name;
+	data = req.body.data;
+	jsonfile.writeFile("data/ann/"+fileName+".json", data, function (err) {
+		if(err == null){
+			res.json({"data": "success"});
+		}
+		else{
+  			res.json({"data": "error"});
+  		}
+	});
+
+});
 
 module.exports = router;
