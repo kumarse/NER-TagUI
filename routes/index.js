d346460c6d801366bff88d2421b2b86afec80fcf
satheeshravir <satheeshravir@gmail.com>
2016-04-07 20:38:10 -0700

    Added file retrieval

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NER Tagger' });
 });
+
+router.get('/list', function(req, res, next) {
+	var myJsonString = ""
+	fs.readdir('data/raw', function(err, items) {
+		myJsonString = JSON.stringify(items);
+		res.render('list', { data: JSON.parse(myJsonString) });	
+	});
+  	
+});
+
 
 module.exports = router;
